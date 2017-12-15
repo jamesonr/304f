bddbfa2f562ae9ca9ef7c3956d7542f60efec44d
Robert Jameson <jamesonr@uni.coventry.ac.uk>
2017-12-15 01:22:55 +0000

    some angular validation added to front + testing double passwords

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -8,25 +8,80 @@ angular.module('userControllers', ['userServices'])
       app.loading = true;
       app.errorMsg = false;
 
-        if (valid) {
-          User.create(app.regData).then(function(data) {
-            if (data.data.success) {
-              app.loading = false;
-              //Success message
-              app.successMsg = data.data.message + '...Redirecting';
-              //Homepage Redirect
-              $timeout(function() {
-                $location.path('/');
-              }, 2500); //adds delay to redirect
-            } else {
-              //Create error Message
-              app.loading = false;
-              app.errorMsg = data.data.message;
-            }
-          });
-        } else {
-          app.loading = false;
-          app.errorMsg = 'Ensure form is filled out properly';
-        }
+      if (valid) {
+        User.create(app.regData).then(function(data) {
+          if (data.data.success) {
+            app.loading = false;
+            //Success message
+            app.successMsg = data.data.message + '...Redirecting';
+            //Homepage Redirect
+            $timeout(function() {
+              $location.path('/');
+            }, 2500); //adds delay to redirect
+          } else {
+            //Create error Message
+            app.loading = false;
+            app.errorMsg = data.data.message;
+          }
+        });
+      } else {
+        app.loading = false;
+        app.errorMsg = 'Ensure form is filled out properly';
+      }
     };
-  });
+
+    // checkUsername(regData)
+    this.checkUsername = function(regData) {
+      app.checkingUsername = true;
+      app.usernameMsg = false;
+      app.usernameInvalid = false;
+
+      User.checkUsername(app.regData).then(function(data) {
+        if (data.data.success) {
+          app.checkingUsername = false;
+          app.usernameInvalid = false;
+          app.usernameMsg = data.data.message;
+        } else {
+          app.checkingUsername = false;
+          app.usernameInvalid = true;
+          app.usernameMsg = data.data.message;
+        }
+      });
+    };
+    this.checkEmail = function(regData) {
+      app.checkingEmail = true;
+      app.emailMsg = false;
+      app.emailInvalid = false;
+
+      User.checkEmail(app.regData).then(function(data) {
+        if (data.data.success) {
+          app.checkingEmail = false;
+          app.emailInvalid = false;
+          app.emailMsg = data.data.message;
+        } else {
+          app.checkingEmail = false;
+          app.emailInvalid = true;
+          app.emailMsg = data.data.message;
+        }
+      });
+    }
+  })
+
+
+      .directive('match', function() {
+        return {
+          restrict: 'A', // Restrict to HTML Attribute
+          controller: function($scope) {
+
+            $scope.doConfirm = function(values) {
+              console.log(values);
+            }
+          },
+
+          link: function(scope, element, attrs) {
+            attrs.$observe('match', function() {
+              scope.doConfirm(attrs.match);
+            });
+          }
+        };
+      })
--- public/app/services/userservices.js
+++ public/app/services/userservices.js
@@ -8,6 +8,15 @@ angular.module('userServices', [])
     userFactory.create = function(regData) {
       return $http.post('/api/users', regData);
     }
+    // User.checkUsername(regData);
+    userFactory.checkUsername = function(regData) {
+      return $http.post('/api/checkusername', regData);
+    }
+
+    // User.checkEmail(regData)
+    userFactory.checkEmail = function(regData) {
+      return $http.post('/api/checkemail', regData);
+    }
 
     return userFactory;
   });
