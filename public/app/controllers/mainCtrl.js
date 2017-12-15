6c4bfd64ea183c8bbcc54b8365d2d07256a7dc63
Robert Jameson <jamesonr@uni.coventry.ac.uk>
2017-12-15 05:01:39 +0000

    MAJOR PROBLEM WITH TIME STAMP

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,11 +1,95 @@
 angular.module('mainController', ['authServices'])
-
-  .controller('mainCtrl', function(Auth, $timeout, $location, $rootScope) {
+  //Main controller runs things that should run on every page
+  .controller('mainCtrl', function(Auth, $timeout, $location, $rootScope, $window, $interval, $route) {
     var app = this;
+    app.loadme = false; //hides html until data obtained by angular
 
-    app.loadme = false;
+    app.checkSession = function() {
+      if (Auth.isLoggedIn()) {
+        app.checkingSession = true;
+        var interval = $interval(function() {
+          var token = $window.localStorage.getItem('token');
+          if (token === null) {
+            $interval.cancel(interval);
+          } else {
+            //grabs time stamp
+            self.parseJwt = function(token) {
+              var base64Url = token.split('.')[1];
+              var base64 = base64Url.replace('-', '+').replace('_', '/');
+              return JSON.parse($window.atob(base64));
+            }
+            //converts time stamp
+            var expireTime = self.parseJwt(token);
+            var timeStamp = Math.floor(Date.now() / 1000);
+            console.log(expireTime.exp);
+            console.log(timeStamp);
+            var timeCheck = expireTime.exp - timeStamp;
+            console.log('timecheck: ' + timeCheck);
+            if (timeCheck <= 10) {
+              console.log('token has expired');
+              showModal(1);
+              $interval.cancel(interval);
+            } else {
+              console.log('token not yet expired');
+            }
+          }
+        }, 2000); //running timestamp interval every 2 secs
+      }
 
+    };
+
+    app.checkSession();
+
+    var showModal = function(option) {
+      app.choiceMade = false;
+      app.modalHeader = undefined;
+      // app.modalBody = false;
+      app.hideButton = false;
+
+      if (option === 1) {
+        app.modalHeader = 'Timeout Warning';
+        app.modalBody = 'Your session will expire in 5 minutes. Would you like to renew?';
+        $("#myModal").modal({
+          backdrop: "static"
+        });
+      } else if (option === 2) {
+        //logout portion
+        app.hideButton = true;
+        app.modalHeader= 'Logging Out';
+        $("#myModal").modal({
+          backdrop: "static"
+        });
+        $timeout(function{
+            Auth.logout();
+            $location.path('/');
+            $route.reload();
+        }, 2000);
+      }
+      $timeout(function() {
+        if (!app.choiceMade) {
+          console.log('Logged Out');
+          hideModal();
+        }
+      }, 4000);
+    };
+
+    app.renewSession = function() {
+      app.choiceMade = true;
+      console.log('session has been renewed');
+      hideModal();
+    };
+    app.endSession = function() {
+      app.choiceMade = true;
+      console.log('session has ended');
+      hideModal();
+    };
+    var hideModal = function() {
+      $("#myModal").modal('hide');
+    };
     $rootScope.$on('$routeChangeStart', function() {
+
+      if (!app.checkSession) app.checkSession();
+
       if (Auth.isLoggedIn()) {
         console.log('Sucess: user logged in');
         app.isLoggedIn = true;
@@ -48,11 +132,8 @@ angular.module('mainController', ['authServices'])
         }
       });
     };
-    this.logout = function() {
-      Auth.logout();
-      $location.path('/logout');
-      $timeout(function() {
-        $location.path('/');
-      }, 2500);
+    // Logout user
+    app.logout = function() {
+      showModal(2);
     };
   });
